b5d3bab7b050f9487fcf10fe68ad7980b3d5087d
MostakemHossain <mostakememon123@gmail.com>
2023-09-29 09:09:17 +0600

    mobile-view-search-toggler-added

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,5 +1,28 @@
+"use strict";
+
+import { fetchData, url } from "./api.js";
+
+import * as module from "./module.js";
+
 /**
- * @license MIT
- * @copyright codewithsadee 2023 All rights reserved
- * @author codewithsadee <mohammadsadee24@gmail.com>
+ *
+ * @param {NodeList} elements Elements node array
+ * @param {string} eventType Event Type "click","mouseover"
+ * @param {Function} callback callback function
  */
+const addEventOnElements = function (elements, eventType, callback) {
+  for (const element of elements) element.addEventListener(eventType, callback);
+};
+/**
+ * Toogle search in mobile devices
+ */
+
+const searchView = document.querySelector("[date-search-view]");
+
+const searchTogglers = document.querySelectorAll("[date-search-toggler]");
+
+const toggleSearch = () => {
+  searchView.classList.toggle("active");
+};
+
+addEventOnElements(searchTogglers, "click", toggleSearch);
